2713332040daac17a940b306994cce55f644e78c
Rahul <rahulkotr1@gmail.com>
2022-04-18 23:04:23 +0530

    Define window dimensions

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from tkinter import *
 
 root=Tk()
+#Override settings
 root.configure(bg="black")
 root.geometry('480x640')
 root.title("Minesweeper")
 root.resizable(False,False)
+
+
+topFrame=Frame(
+    root,
+    bg="white",
+    width=480,
+    height=640
+    )
+
+
+
+
+
 root.mainloop()
