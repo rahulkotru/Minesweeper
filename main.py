f0a9367355ee3cc2224a8d76500ad3dde1dd7104
Rahul <rahulkotr1@gmail.com>
2022-04-18 23:00:55 +0530

    Define window color

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 
 root=Tk()
-root.geometry('1440x720')
+root.configure(bg="black")
+root.geometry('480x640')
 root.title("Minesweeper")
+root.resizable(False,False)
 root.mainloop()
